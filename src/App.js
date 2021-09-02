6ce576712b043d8812fdb93f2f2d32f43f997a0d
RamonCruz-Hinojosa <Bairdwins@gmail.com>
2021-09-02 12:03:26 -0500

    Only the delet button left to do

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
-import "./App.css";
+import React from "react";
 
-function App() {
-  return <div className="App"></div>;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: "",
+      todos: [],
+      isClicked: false,
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      text: event.target.value,
+    });
+  };
+
+  handleClick = (event) => {
+    event.preventDefault();
+    this.setState({
+      todos: [...this.state.todos, this.state.text],
+    });
+    this.setState({
+      text: "",
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.handleClick}>
+          <input
+            onChange={this.handleChange}
+            type="text"
+            value={this.state.text}
+          ></input>
+          <button type="submit">Add Todos</button>
+        </form>
+        <ol>
+          {this.state.todos.map((todo, index) => {
+            return <li key={index}>{todo}</li>;
+          })}
+        </ol>
+      </div>
+    );
+  }
 }
 
 export default App;
